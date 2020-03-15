344faaa913d26a4e89694aa149cf92b2fbd03fae
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-15 16:47:37 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        {name: "Benvolio"}
       ]
    }, 
    :capulet => {
